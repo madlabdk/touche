de68e1d41b757617f38dbc905c13aeaf7e8689d1
Tats <info@sofianaudry.com>
2014-04-10 23:34:39 -0400

    Added compatibility with Arduino Mega

--- arduino/touche_graph/touche_graph.ino
+++ arduino/touche_graph/touche_graph.ino
@@ -2,20 +2,38 @@
 
 void setup ()
 {
-  pinMode (9, OUTPUT); 
+  pinMode (9, OUTPUT);
+
+#if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__))
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCCR2A |= (1 << COM2B0);   // Toggle OC2A on Compare Match.
+  TCCR2A |= (1 << WGM21);    // CTC mode
+#else
   TCCR1A = 0;
   TCCR1B = 0;
   TCCR1A |= (1 << COM1A0);   // Toggle OC1A on Compare Match.
   TCCR1B |= (1 << WGM12);    // CTC mode
+#endif
+
   Serial.begin(57600);
 }
 
 void loop () {
   for (int i = 0; i < steps; i++) {
+
+#if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__))
+    TCCR2B &= 0xFE; // turns off timer
+    TCNT2 = 0;      // resets timer counter register
+    OCR2A = i;      // changes frequency step
+    TCCR2B |= 0x01; // turns on timer
+#else
     TCCR1B &= 0xFE; // turns off timer
     TCNT1 = 0;      // resets timer counter register
     OCR1A = i;      // changes frequency step
     TCCR1B |= 0x01; // turns on timer
+#endif
+
     Serial.print(i,DEC);
     Serial.print(" ");
     Serial.println(analogRead(0), DEC);
--- arduino/touche_peak/touche_peak.ino
+++ arduino/touche_peak/touche_peak.ino
@@ -7,10 +7,19 @@ int maxPos, maxVal;
 void setup ()
 {
   pinMode (9, OUTPUT); 
+
+#if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__))
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCCR2A |= (1 << COM2B0);   // Toggle OC2A on Compare Match.
+  TCCR2A |= (1 << WGM21);    // CTC mode
+#else
   TCCR1A = 0;
   TCCR1B = 0;
-  TCCR1A |= (1 << COM1A0);        // Toggle OC1A on Compare Match.
-  TCCR1B |= (1 << WGM12);         // CTC mode
+  TCCR1A |= (1 << COM1A0);   // Toggle OC1A on Compare Match.
+  TCCR1B |= (1 << WGM12);    // CTC mode
+#endif
+
   Serial.begin(57600);
 }
 
@@ -21,10 +30,19 @@ void loop () {
   maxPos = 0;
   maxVal = 0;
   for (int i = 0; i < steps; i++) {
-    TCCR1B &= 0xFE;                       // turns off timer
-    TCNT1 = 0;                            // resets timer counter register
-    OCR1A = i;                            // sets new frequency step
-    TCCR1B |= 0x01;                       // turns on timer
+
+#if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__))
+    TCCR2B &= 0xFE; // turns off timer
+    TCNT2 = 0;      // resets timer counter register
+    OCR2A = i;      // changes frequency step
+    TCCR2B |= 0x01; // turns on timer
+#else
+    TCCR1B &= 0xFE; // turns off timer
+    TCNT1 = 0;      // resets timer counter register
+    OCR1A = i;      // changes frequency step
+    TCCR1B |= 0x01; // turns on timer
+#endif
+
     float curVal = analogRead(0);
     values[i] = values[i] * alpha + curVal * (1 - alpha);  // exponential moving avg
     if (values[i] > maxVal) {                              // finds the signal peak
